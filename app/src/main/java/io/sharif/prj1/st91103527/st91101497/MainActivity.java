cd8d0a9a0c6eb632b5a082a8f36aab62bf5472a6
esmaeil4141 <esmaeil414@gmail.com>
2016-04-06 22:33:12 +0430

    esmaeil

--- app/src/main/java/io/sharif/prj1/st91103527/st91101497/MainActivity.java
+++ app/src/main/java/io/sharif/prj1/st91103527/st91101497/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        LinearLayout l=(LinearLayout)findViewById(R.id.sample_main_layout);
+        TextView tv=new TextView(getApplicationContext());
+        tv.setText("alakieeee!!!");
+        l.addView(tv);
     }
 
 }

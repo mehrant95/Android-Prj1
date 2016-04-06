d596b458e514b066ee3fa70f13128e14512020d6
M3hRaN <m3hrantorki@gmail.com>
2016-04-06 20:05:12 +0330

    first commit

---
+++ app/src/main/java/io/sharif/prj1/st91103527/st91101497/MainActivity.java
@@ -0,0 +1,19 @@
+package io.sharif.prj1.st91103527.st91101497;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
